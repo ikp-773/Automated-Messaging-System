a749a954d6705cceafdfd6bafd4532e437afd120
darklord773 <57410559+darklord773@users.noreply.github.com>
2020-08-01 22:20:14 +0530

    update

--- automationWA.py
+++ automationWA.py
@@ -4,14 +4,14 @@ import time
 driver = webdriver.Chrome('./chromedriver')
 driver.get('https://web.whatsapp.com')
 input('Press anything to start.  ')
-fhandle = open('allstars.txt')
+fhandle = open('allstars.txt')  #
 data = fhandle.read()
 lines = data.split('\n')
 i = 0
 
 for line in lines:
     search = driver.find_element_by_xpath('//*[@id="side"]/div[1]/div/label/div/div[2]')
-    search.send_keys('Contact Name')
+    search.send_keys('Contact Name')  # Enter the name of a contact on your phone
     time.sleep(1)
     name = driver.find_element_by_class_name('_2kHpK')
     name.click()
